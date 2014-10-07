9111491ab5672226926d63ba64caa74bc99365e9
Bargetor <bargetor@gmail.com>
2014-10-08 00:36:26 +0800

    添加github联系方式

--- wp-content/themes/bargetor/footer.php
+++ wp-content/themes/bargetor/footer.php
@@ -12,18 +12,23 @@
 		<div class="footer">
 			<div class="container contact-div">
 				<div class="row">
-					<div class="col-xs-4 col-sm-4 col-md-4 text-center">
+					<div class="col-xs-3 col-sm-3 col-md-3 text-center">
 						<a class="contact-weibo" href="http://weibo.com/bargetor" title="@神经病患者蓝桥">
 							<div></div>
 						</a>
 					</div>
-					<div class="col-xs-4 col-sm-4 col-md-4 text-center">
+					<div class="col-xs-3 col-sm-3 col-md-3 text-center">
 						<a class="contact-E-mail" href="mailto:madign@qq.com" title="写信给我">
 							<div></div>
 						</a>
 					</div>
-					<div class="col-xs-4 col-sm-4 col-md-4 text-center">
+					<div class="col-xs-3 col-sm-3 col-md-3 text-center">
 						<a class="contact-QQ" href="http://wpa.qq.com/msgrd?v=3&uin=284307306&site=qq&menu=yes" title="与我聊天-284307306">
+							<div></div>
+						</a>
+					</div>
+					<div class="col-xs-3 col-sm-3 col-md-3 text-center">
+						<a class="contact-github" href="https://github.com/Bargetor" title="github">
 							<div></div>
 						</a>
 					</div>
