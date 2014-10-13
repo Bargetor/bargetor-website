2605be858f049cd60c10b7d1fde78bd54a9b8eb3
Bargetor <bargetor@gmail.com>
2014-10-13 16:13:53 +0800

    add chestnut rewrite rule
    添加chestnut跳转规则代理

--- wp-content/themes/bargetor/custom/query/BargetorRewriteRuleProxy.php
+++ wp-content/themes/bargetor/custom/query/BargetorRewriteRuleProxy.php
@@ -4,31 +4,32 @@
   	* 重写规则请求名称常量
   	*/
  	define('BARGETOR_QUERY_VAR_NAME', 'bargetor_custom_page');
- 	
+
 	class BargetorRewriteRuleProxy{
 		private static $instance;
 		public $target;
-		
+
 		/*
   		* 重写映射数组，只支持第一级
   		* ex.www.bargetor.com/aboutme
   		*/
 		private static  $array = array(
- 		'aboutme' => '/about-me.php', 
+ 		'aboutme' => '/about-me.php',
  		'products' => '/products.php',
 		'works' => '/products.php',
- 		'life' => '/life.php');
-		
+ 		'life' => '/life.php',
+                        'api'=> '/chestnut/api_proxy.php');
+
 		private function __construct(){
 			$this->target = new BaseBargetorRewriteRule(BARGETOR_QUERY_VAR_NAME, self::$array);
 		}
-		
+
 		public static function getInstance(){
 			if(!(self::$instance instanceof self)){
 			self::$instance = new self;
 			}
 			return self::$instance;
 		}
-		
+
 	}
 ?>
