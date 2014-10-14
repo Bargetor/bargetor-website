fa9797f4ae6722cbb14fe744e72a52be8286d88e
Bargetor <bargetor@gmail.com>
2014-10-14 10:59:56 +0800

    contact for new window
    联系方式改为打开新窗口

--- wp-content/themes/bargetor/footer.php
+++ wp-content/themes/bargetor/footer.php
@@ -13,22 +13,22 @@
 			<div class="container contactdiv">
 				<div class="row">
 					<div class="col-xs-3 col-sm-3 col-md-3 text-center">
-						<a class="contact-weibo" href="http://weibo.com/bargetor" title="@神经病患者蓝桥">
+						<a class="contact-weibo" href="http://weibo.com/bargetor" title="@神经病患者蓝桥" target="_blank">
 							<span class="icon-contact-weibo"></span>
 						</a>
 					</div>
 					<div class="col-xs-3 col-sm-3 col-md-3 text-center">
-						<a class="contact-E-mail" href="mailto:madign@qq.com" title="写信给我">
+						<a class="contact-E-mail" href="mailto:madign@qq.com" title="写信给我" target="_blank">
 							<span class="icon-contact-email"></span>
 						</a>
 					</div>
 					<div class="col-xs-3 col-sm-3 col-md-3 text-center">
-						<a class="contact-QQ" href="http://wpa.qq.com/msgrd?v=3&uin=284307306&site=qq&menu=yes" title="与我聊天-284307306">
+						<a class="contact-QQ" href="http://wpa.qq.com/msgrd?v=3&uin=284307306&site=qq&menu=yes" title="与我聊天-284307306" target="_blank">
 							<span class="icon-contact-qq"></span>
 						</a>
 					</div>
 					<div class="col-xs-3 col-sm-3 col-md-3 text-center">
-						<a class="contact-github" href="https://github.com/Bargetor" title="github">
+						<a class="contact-github" href="https://github.com/Bargetor" title="github" target="_blank">
 							<span class="icon-contact-github"></span>
 						</a>
 					</div>
@@ -38,7 +38,7 @@
 		</div>
 		<!-- bootstrap javascript -->
 		<script src="<?php bloginfo('template_url');?>/js/bootstrap.min.js"></script>
-		
+
     </body>
 
 </html>
