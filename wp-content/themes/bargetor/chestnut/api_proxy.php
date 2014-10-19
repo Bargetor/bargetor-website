cf4e9dc43c8cbff0c6e1533002990e9ce40c8ee6
Bargetor <bargetor@gmail.com>
2014-10-19 14:21:46 +0800

    api proxy modify

--- wp-content/themes/bargetor/chestnut/api_proxy.php
+++ wp-content/themes/bargetor/chestnut/api_proxy.php
@@ -5,87 +5,32 @@
 
 //define your token
 define("TOKEN", "bargetor_chestnut");
-define("target_url", "http://127.0.0.1:8000/");
+define("TARGET_URL", "http://localhost:8000/");
 
-$wechatObj = new wechatCallbackapiTest();
-$wechatObj->valid();
+echo chectnut_api_proxy();
 
-class wechatCallbackapiTest
-{
-	public function valid()
-	{
-		$echoStr = $_GET["echostr"];
 
-		//valid signature , option
-		if($this->checkSignature()){
-			echo $echoStr;
-			exit;
-		}
+function chectnut_api_proxy(){
+	$methon = $_SERVER['REQUEST_METHOD'];
+	if($methon == 'GET'){
+		$params = build_query($_GET);
+		return http_query_get(TARGET_URL, $params);
+		
 	}
-
-	public function responseMsg()
-	{
-		//get post data, May be due to the different environments
-		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-
-		//extract post data
-		if (!empty($postStr)){
-			/* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
-			 the best way is to check the validity of xml by yourself */
-			libxml_disable_entity_loader(true);
-			$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-			$fromUsername = $postObj->FromUserName;
-			$toUsername = $postObj->ToUserName;
-			$keyword = trim($postObj->Content);
-			$time = time();
-			$textTpl = "<xml>
-                            <ToUserName><![CDATA[%s]]></ToUserName>
-                            <FromUserName><![CDATA[%s]]></FromUserName>
-                            <CreateTime>%s</CreateTime>
-                            <MsgType><![CDATA[%s]]></MsgType>
-                            <Content><![CDATA[%s]]></Content>
-                            <FuncFlag>0</FuncFlag>
-                            </xml>";
-			if(!empty( $keyword ))
-			{
-				$msgType = "text";
-				$contentStr = "Welcome to wechat world!";
-				$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-				echo $resultStr;
-			}else{
-				echo "Input something...";
-			}
-
-		}else {
-			echo "";
-			exit;
-		}
+	if($methon == 'POST'){
+		
 	}
-
-	private function checkSignature()
-	{
-		// you must define TOKEN by yourself
-		if (!defined("TOKEN")) {
-			throw new Exception('TOKEN is not defined!');
-		}
-
-		$signature = $_GET["signature"];
-		$timestamp = $_GET["timestamp"];
-		$nonce = $_GET["nonce"];
-
-		$token = TOKEN;
-		$tmpArr = array($token, $timestamp, $nonce);
-		// use SORT_STRING rule
-		sort($tmpArr, SORT_STRING);
-		$tmpStr = implode( $tmpArr );
-		$tmpStr = sha1( $tmpStr );
-
-		if( $tmpStr == $signature ){
-			return true;
-		}else{
-			return false;
-		}
-	}
+	
 }
+
+
+function http_query_get($url, $params){
+	return file_get_contents($url . '?' . $params);
+}
+
+function http_query_post($url, $params){
+	
+}
+
 
 ?>
