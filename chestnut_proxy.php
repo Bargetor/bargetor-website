c15b5212c2d17a908a0ed6c06474b2623857b863
Bargetor <bargetor@gmail.com>
2014-10-30 21:05:04 +0800

    部分函数优化

--- chestnut_proxy.php
+++ chestnut_proxy.php
@@ -34,7 +34,7 @@ function http_query_get($url, $params){
     //return file_get_contents($url . '?' . $params);
 }
 
-function http_query_post($url, $params){
+function http_query_post($url, $params, $content_type = 'text/xml'){
     $curlObj = curl_init();
     $options = array(
     CURLOPT_URL => $url,
@@ -42,7 +42,7 @@ function http_query_post($url, $params){
     CURLOPT_RETURNTRANSFER => TRUE, //接收服务端范围的html代码而不是直接浏览器输出
     CURLOPT_TIMEOUT => 4,
     CURLOPT_POSTFIELDS => $params, //post的数据
-    CURLINFO_CONTENT_TYPE => 'text/xml'
+    CURLINFO_CONTENT_TYPE => $content_type
     );
     curl_setopt_array($curlObj, $options);
     $response = curl_exec($curlObj);
