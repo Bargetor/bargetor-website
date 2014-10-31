a9c319505c9404bc0d8631bb5e04a1fe1d78db60
Bargetor <bargetor@gmail.com>
2014-10-31 21:46:44 +0800

    ..

--- wp-config.php
+++ wp-config.php
@@ -15,16 +15,16 @@
 
 // ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
 /** WordPress数据库的名称 */
-define('DB_NAME', 'bargetor');
+define('DB_NAME', 'bargetor_website');
 
 /** MySQL数据库用户名 */
 define('DB_USER', 'bargetor_admin');
 
 /** MySQL数据库密码 */
-define('DB_PASSWORD', 'bargetor');
+define('DB_PASSWORD', 'majinluyi');
 
 /** MySQL主机 */
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', 'localhost');
 /** 创建数据表时默认的文字编码 */
 define('DB_CHARSET', 'utf8');
 
