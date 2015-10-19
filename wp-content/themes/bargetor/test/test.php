8b84f2853710bcde0462f18b8e8dd9d1f9b97973
Bargetor <bargetor@gmail.com>
2015-10-19 19:08:20 +0800

    ..

--- wp-content/themes/bargetor/test/test.php
+++ wp-content/themes/bargetor/test/test.php
@@ -2,7 +2,7 @@
 	$method = $_SERVER['REQUEST_METHOD'];
     $params = build_query($_GET);
     $postStr = build_query($_POST);
-    $log_str = 'the request is ' . $method . 'the params is ||'. $params . 'and request body is || ' . $postStr);
-    error_log($log_str, 3, '/var/log/web/bargetor-website.log');
+    $log_str = "the request is " . $method . "the params is ||". $params . "and request body is || " . $postStr);
+    error_log($log_str, 3, "/var/log/web/bargetor-website.log");
 
 ?>
