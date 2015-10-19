379f0337c29d962be460e3994060a7c04cc1023a
Bargetor <bargetor@gmail.com>
2015-10-19 18:29:36 +0800

    ...

--- wp-content/themes/bargetor/test/test.php
+++ wp-content/themes/bargetor/test/test.php
@@ -1,8 +1,10 @@
 <?php
 	$method = $_SERVER['REQUEST_METHOD'];
     echo $method;
-    echo '</br>'
+    echo '</br>';
     $params = build_query($_GET);
     echo $params;
     echo '</br>';
+
+    echo $_POST;
 ?>
