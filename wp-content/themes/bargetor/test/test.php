f32d9cc03465b272c607657319030d6fa928a304
Bargetor <bargetor@gmail.com>
2015-10-19 19:05:35 +0800

    ..

--- wp-content/themes/bargetor/test/test.php
+++ wp-content/themes/bargetor/test/test.php
@@ -7,7 +7,7 @@
     echo '</br>';
 
     echo $_POST;
-    $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+    $postStr = build_query($_POST);
     $log_str = 'the request is ' . $method . 'the params is ||'. $params . 'and request body is || ' . $postStr);
     error_log($log_str, 3, '/var/log/web/bargetor-website.log');
 
