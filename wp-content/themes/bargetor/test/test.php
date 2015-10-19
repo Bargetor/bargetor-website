f26bd0e363ac9bcd2889f39eb49422b21e19b418
Bargetor <bargetor@gmail.com>
2015-10-19 18:43:26 +0800

    ..

--- wp-content/themes/bargetor/test/test.php
+++ wp-content/themes/bargetor/test/test.php
@@ -9,6 +9,6 @@
     echo '</br>';
 
     echo $_POST;
-
-    error_log('the request is ' . $method . 'the params is ||'. $params . 'and request body is || ' . $_POST, 3, LOG_FILE);
+    echo 'the request is ' . $method . 'the params is ||'. $params . 'and request body is || ' . $_POST, 3
+    error_log('the request is ', 3, LOG_FILE);
 ?>
