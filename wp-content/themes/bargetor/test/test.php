b2b2330fe44ddc351e4634adb0c9a7eb6b767062
Bargetor <bargetor@gmail.com>
2015-10-19 18:59:18 +0800

    ..

--- wp-content/themes/bargetor/test/test.php
+++ wp-content/themes/bargetor/test/test.php
@@ -7,8 +7,8 @@
     echo '</br>';
 
     echo $_POST;
-
-    $log_str = 'the request is ' . $method . 'the params is ||'. $params . 'and request body is || ' . $_POST;
+    $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+    $log_str = 'the request is ' . $method . 'the params is ||'. $params . 'and request body is || ' . $postStr);
     error_log($log_str, 3, '/var/log/web/bargetor-website.log');
 
 ?>
