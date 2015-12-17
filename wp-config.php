fbfaf3da78bf1b6d5b47835316c83acc09841c35
Bargetor <bargetor@gmail.com>
2015-12-17 11:11:39 +0800

    呵呵呵，被黑了

--- wp-config.php
+++ wp-config.php
@@ -15,16 +15,16 @@
 
 // ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
 /** WordPress数据库的名称 */
-define('DB_NAME', 'bargetor_website');
+define('DB_NAME', 'bargetor');
 
 /** MySQL数据库用户名 */
 define('DB_USER', 'bargetor_admin');
 
 /** MySQL数据库密码 */
-define('DB_PASSWORD', 'majinluyi');
+define('DB_PASSWORD', 'CJaQko66b8ADj4cA434k47Y6');
 
 /** MySQL主机 */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '127.0.0.1');
 /** 创建数据表时默认的文字编码 */
 define('DB_CHARSET', 'utf8');
 
